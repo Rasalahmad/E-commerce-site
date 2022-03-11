862103052ef123ebb9ac62275d4f86904897ffe7
Rasalahmad <rasalahmadsit@gmail.com>
2022-03-11 10:22:13 +0600

    project setup

---
+++ src/routes/userRoutes.tsx
@@ -0,0 +1,7 @@
+const publicRoutes = [
+  {
+    path: "/profile",
+    name: "Profile",
+    // component: <Profile />
+  },
+];
---
+++ src/views/pages/TheLayout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Footer from "./common/Footer";
+import Header from "./common/Header";
+import Main from "./common/Main";
+
+export default function TheLayout() {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <main>
+        <Main />
+      </main>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
